0e226e2c5e9fab40f9e42e759db73592f58a32be
KaleIsProgramming <pio1kostecki@gmail.com>
2022-09-05 20:23:31 +0200

    Day_7
    Feat: Added validation for Custom Color Picker in rgb and hex model

--- src/components/customColorPopUp.tsx
+++ src/components/customColorPopUp.tsx
@@ -1,12 +1,96 @@
+import { useRef } from "react";
 import styled from "@emotion/styled";
 import { useAppDispatch } from "../store";
 import { pickCustomColor } from "../store/slices";
+import { changeColor } from "../store/slices";
 
 export const CustomColorPopUp = () => {
+    const inputR = useRef<HTMLInputElement>(null);
+    const inputG = useRef<HTMLInputElement>(null);
+    const inputB = useRef<HTMLInputElement>(null);
+    const inputHex = useRef<HTMLInputElement>(null);
     const dispatch = useAppDispatch();
 
     const closePopUpHandler = () => {
         dispatch(pickCustomColor());
+    }
+
+    const changeColorHandler = (action: string) => {
+
+        switch(action){
+            case 'rgb':
+                const isRgbRight = validationHandler('rgb');
+                if(isRgbRight) {
+                    const RgbPayload = {color: `rgb(${inputR.current?.value}, ${inputG.current?.value}, ${inputB.current?.value})`};
+                    dispatch(changeColor(RgbPayload));
+                }
+                break;
+            case 'hex':
+                const isHexRight = validationHandler('hex');
+                if(isHexRight) {
+                    const hexPayload = {color: `${inputHex.current?.value}`}
+                    dispatch(changeColor(hexPayload));
+                }
+                break;
+        }
+    }
+
+    const validationHandler = (action: string) => {
+        const hexChars = "#0123456789abcdef";
+        const rgbChars = "0123456789";
+
+        switch(action) {
+            case 'rgb':
+                const inputs = [inputR.current!, inputG.current!, inputB.current!];
+
+                if((inputR.current!.value.length > 3) || (inputG.current!.value.length > 3) || (inputB.current!.value.length > 3)) {
+                    inputR.current!.value = '255';
+                    inputG.current!.value = '255';
+                    inputB.current!.value = '255';
+                    return false;
+                } else {
+                    let isRgbRight = true;
+                    for (let i = 0; i < inputs.length; i++){
+                        for(let j = 0; j < inputs[i].value.length; j++){
+                            if(!(inputs[i].value.includes(rgbChars))) {
+                                isRgbRight = false;
+                            }
+                        }
+
+                        if(!isRgbRight){
+                            inputs[i].value = "255";
+                        }
+                    }
+                    return isRgbRight;
+                }
+            case 'hex':
+                if(inputHex.current!.value.length > 7) {
+                    inputHex.current!.value = '#ffffff';
+                    return false;
+                } else {
+                    const length = inputHex.current!.value.length;
+                    let isHexRight = true;
+
+                    for(let i = 0; i < length; i++) {
+                        if(!(hexChars.includes(inputHex.current!.value[i]))) {
+                            isHexRight = false;
+                        }
+                    }
+
+                    if(!isHexRight) {
+                        inputHex.current!.value = "#ffffff";
+                    }
+
+                    return isHexRight;
+                }
+        }
+    }
+
+    const errorHandler = (errorMessage: string) => {
+        switch(errorMessage){
+            case "":
+                break;
+        }
     }
 
     return(
@@ -24,18 +108,18 @@ export const CustomColorPopUp = () => {
                     <h2>Model RGB:</h2>
                     <InputsContainer>
                         <p>R:</p>
-                        <input type="text" />
+                        <input ref={inputR} type="text" defaultValue={0} />
                         <p>G:</p>
-                        <input type="text" />
+                        <input ref={inputG} type="text" defaultValue={0} />
                         <p>B:</p>
-                        <input type="text" />
-                        <button>Submit</button>
+                        <input ref={inputB} type="text" defaultValue={0} />
+                        <button onClick={() => changeColorHandler('rgb')}>Submit</button>
                     </InputsContainer>
                 </RgbPickingContainer>
                 <HexPickingContainer>
                     <h2>Model Hex:</h2>
-                    <input type="text" />
-                    <button>Submit</button>
+                    <input ref={inputHex} type="text" defaultValue={"#ffffff"} />
+                    <button onClick={() => changeColorHandler('hex')}>Submit</button>
                 </HexPickingContainer>
             </ContentContainer>    
         </PopUpContainer>
